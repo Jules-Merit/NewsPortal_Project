52af8eba896a20adb4321b8e090ec39a8193a9f6
Jules_Merit <grigory.tuchinov@yandex.ru>
2023-04-12 16:37:24 +1000

    newsportal app

---
+++ newsportal/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Author(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Post(models.Model):
+    pass
+
+
+class PostCategory(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
